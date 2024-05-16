5b4b2558a3c3a428825d24746a3a9a9e832196ea
Jason Sianandar <sianandarjason@gmail.com>
2024-05-16 16:15:02 +0700

    1/5

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,55 @@
+// import Link from "next/link";
+import styles from './links.module.css'
+import NavLink from "./navLink/navLink";
+
+
+const Links = () => {
+    const links = [
+        {
+            title: "Homepage",
+            path: '/',
+        },
+        {
+            title: "About",
+            path: '/about',
+        },
+        {
+            title: "Contact",
+            path: '/contact',
+        },
+        {
+            title: "Blog", 
+            path: '/blog',
+        },
+    ];
+
+    // TEMPORARY
+    const isAdmin = true;
+    const session = true;
+    
+
+    return (
+        <div className={styles.links}>
+          {links.map((link => 
+            <NavLink item={link} key={link.title}/>
+          ))}
+          {
+            session ? (
+                <>
+                {
+                    isAdmin && (
+                        <NavLink item={{title: "Admin", path: "/admin"}} />
+                    )
+                }
+                <button className={styles.logout}>Logout</button>
+                
+                </>
+            ) : (
+                <NavLink item={{title: "Login", path: "/login"}} />
+            )
+          }
+        </div>
+    );
+}
+
+export default Links;
